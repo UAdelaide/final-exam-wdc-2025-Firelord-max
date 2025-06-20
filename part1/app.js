87762368ded5b5395956386a9f8abcf31a4f77fb
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:10:53 +0000

    [automatic commit] 2025-06-20 05:10:52 UTC 61.245.150.84

--- part1/app.js
+++ part1/app.js
@@ -115,8 +115,13 @@ let db;
         ('Lisa', 'Lissy@mail.com', '1011hash', 'owner');
       `);
     }
-    
-
+    const [rows_2] = await db.execute('SSELECT COUNT(*) AS count FROM Dogs');
+    if (rows2[0].count === 0) {
+      await db.execute(`
+        INSERT INTO Dogs (owner_id, name, size) VALUES
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small');
+      `);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
