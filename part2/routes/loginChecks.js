9df84baff24f1fe3b7695b08ff67277608ea9332
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:10:35 +0000

    [automatic commit] 2025-06-20 10:10:35 UTC ;; communications error to 208.67.222.222#53: timed out 61.245.150.84

--- part2/routes/loginChecks.js
+++ part2/routes/loginChecks.js
@@ -17,17 +17,10 @@ router.post('/login', function (req, res, next) {
         FROM Users
         WHERE Users.password_hash = password
         `);
-    if (!username) {
+    if (!password) {
         res.sendStatus(401);
     }
-    const [rows_4] = await db.execute('SELECT COUNT(*) AS count FROM WalkRatings');
-    if (rows_4[0].count === 0) {
-      await db.execute(`
-        INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-        ((SELECT request_id FROM WalkRequests WHERE request_id = 1), (SELECT user_id FROM Users WHERE username = 'bobwalker'),(SELECT user_id FROM Users WHERE username = 'alice123'), 5, 'Slow dog, but cute'),
-        ((SELECT request_id FROM WalkRequests WHERE request_id = 2), (SELECT user_id FROM Users WHERE username = 'bobwalker'),(SELECT user_id FROM Users WHERE username = 'alice123'), 2, 'Silly dog, ate bunny');
-      `);
-      }
+}
 
 })
 module.exports = router;
