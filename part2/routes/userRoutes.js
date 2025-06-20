f63f3be7913fafa818fa88eac5ac5c490c98d92e
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:56:03 +0000

    [automatic commit] 2025-06-20 13:56:03 UTC 61.245.150.84

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 // GET dogs
 router.get('/getDogs', async (req, res) => {
   if (!req.session.user || req.session.user.role !=== 'owner') {
-    return res.status(500)
+    return res.status(401).json({error : 'Unathorised'}); 
   }
 
   try {
