cd4dabfac7d75472996bdbab2868b45f648d911b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:16:06 +0000

    [automatic commit] 2025-06-20 10:16:06 UTC ;; communications error to 208.67.222.222#53: timed out 61.245.150.84

--- part2/routes/loginChecks.js
+++ part2/routes/loginChecks.js
@@ -15,6 +15,7 @@ router.post('/login', function (req, res, next) {
     }
 
     const user = rows[0];
+    
     password = db.execute(`SELECT Users.password_hash
         FROM Users
         WHERE Users.password_hash = ?
