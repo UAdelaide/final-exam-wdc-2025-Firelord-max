aa14c5b151f05ac979a9f86815dc2276ca22de46
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:03:15 +0000

    [automatic commit] 2025-06-20 10:03:15 UTC 61.245.150.84

--- part2/routes/loginChecks.js
+++ part2/routes/loginChecks.js
@@ -6,5 +6,15 @@ router.post('/login', function (req, res, next) {
     const { username, password } = req.body;
 
     
+
+    const [rows_4] = await db.execute('SELECT COUNT(*) AS count FROM WalkRatings');
+    if (rows_4[0].count === 0) {
+      await db.execute(`
+        INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
+        ((SELECT request_id FROM WalkRequests WHERE request_id = 1), (SELECT user_id FROM Users WHERE username = 'bobwalker'),(SELECT user_id FROM Users WHERE username = 'alice123'), 5, 'Slow dog, but cute'),
+        ((SELECT request_id FROM WalkRequests WHERE request_id = 2), (SELECT user_id FROM Users WHERE username = 'bobwalker'),(SELECT user_id FROM Users WHERE username = 'alice123'), 2, 'Silly dog, ate bunny');
+      `);
+      }
+
 })
 module.exports = router;
