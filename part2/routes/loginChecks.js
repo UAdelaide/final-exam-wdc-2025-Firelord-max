26014d30c9c515fef3861b4687815acb0eacf835
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:22:24 +0000

    [automatic commit] 2025-06-20 10:22:24 UTC 61.245.150.84

--- part2/routes/loginChecks.js
+++ part2/routes/loginChecks.js
@@ -5,31 +5,33 @@ var db = require('../db');
 router.post('/login', async function (req, res, next) {
     const { username, password } = req.body;
 
-    const [rows] = await db.execute(`SELECT Users.username
+    try {
+        const [rows] = await db.execute(`SELECT Users.username
         FROM Users
         WHERE Users.username = ?`,
         [username]);
 
-    if (rows.length === 0) {
-        return res.sendStatus(401);
-    }
+        if (rows.length === 0) {
+            return res.sendStatus(401);
+        }
 
-    const user = rows[0];
+        const user = rows[0];
 
-    if (user.password_hash !== password) {
-        return res.sendStatus(401);
-    }
+        if (user.password_hash !== password) {
+            return res.sendStatus(401);
+        }
 
-    if (user.role === 'owner') {
-        return res.json({redirect: 'owner-dashboard.html'});
-    } else if (user.role === 'walker') {
-        return res.json({redirect: 'walker-dashboard.html'});
-    } else {
-        return res.status(400).send('Unknown role');
-    }
-    catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-    }
+        if (user.role === 'owner') {
+            return res.json({redirect: 'owner-dashboard.html'});
+        } else if (user.role === 'walker') {
+            return res.json({redirect: 'walker-dashboard.html'});
+        } else {
+            return res.status(400).send('Unknown role');
+        }
+        catch (err) {
+            console.error(err);
+            res.sendStatus(500);
+          }
+      }
 });
 module.exports = router;
