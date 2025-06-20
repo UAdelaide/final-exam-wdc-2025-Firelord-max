9be3d403a4b6fc4bc7edf454418077388655e905
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:40:56 +0000

    [automatic commit] 2025-06-20 06:40:56 UTC 61.245.150.84

--- part1/routes/dogsrequests.js
+++ part1/routes/dogsrequests.js
@@ -24,7 +24,7 @@ router.get('/walkrequests/open', async (req, res) => {
 router.get('/walkers/summary', async (req, res) => {
     const[rows] = await db.query(`
         SELECT Users.username, COUNT(WalkRatings.rating_id) AS no_rating, AVG(WalkRatings.rating) AS avg_rating, COUNT(WalkRequests.status = 'completed')
-        FROM USERS
+        FROM Users
         INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
         INNER JOIN WalkRequests ON WalkRatings.request_id = WalkRequests.request_id
         WHERE Users.role = 'walker'
