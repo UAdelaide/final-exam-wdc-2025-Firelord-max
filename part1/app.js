70b0c7ece3b78a56db1f8cba22ce648387343abc
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:03:29 +0000

    [automatic commit] 2025-06-20 04:03:29 UTC 61.245.150.84

--- part1/app.js
+++ part1/app.js
@@ -60,7 +60,7 @@ let db;
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('Benjamin', 'Benny@mail.com', '1234hash', 'walker'),
         ('Robert', 'Rob@mail.com', '1004hash', 'owner'),
-        ('Brave New World', 'Aldous Huxley')
+        ('Lisa', 'Lissy@mail.com', '1004hash', 'owner');
       `);
     }
   } catch (err) {
