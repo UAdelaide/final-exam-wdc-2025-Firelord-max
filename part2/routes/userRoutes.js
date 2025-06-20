2d4dff23a82a10a78430809933a3541c7aa4e61c
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:00:54 +0000

    [automatic commit] 2025-06-20 14:00:54 UTC 61.245.150.84

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -21,7 +21,8 @@ router.get('/getDogs', async (req, res) => {
   try {
     const owner_id = req.session.user_id;
     const [rows] = await db.query(
-      `SELECT name FROM Dogs
+      `SELECT name FROM Dogs WHERE owner_id = ?',
+
       `)
 
   }
