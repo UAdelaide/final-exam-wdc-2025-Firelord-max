f7deec5c37a3ea5e240e47e5abcdf323f8258d29
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:09:36 +0000

    [automatic commit] 2025-06-20 04:09:35 UTC 61.245.150.84

--- part1/app.js
+++ part1/app.js
@@ -50,6 +50,7 @@ let db;
         password_hash VARCHAR(255) NOT NULL,
         role ENUM('owner', 'walker') NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
       )
     `);
 
