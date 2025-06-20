10ffa95a174a7bd7506b3aab285bac003dfad4c0
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:33:29 +0000

    [automatic commit] 2025-06-20 05:33:29 UTC 61.245.150.84

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogrequests = require('./routes/dogrequests')
+var dogrequests = require('./routes/dogrequests');
+app.use('/api', dogrequests);
 
 var app = express();
 
